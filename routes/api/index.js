ef3fe3b554faea79d51a7cd1c35c41e1c241e7f8
ejc10d <112822833+ejc10d@users.noreply.github.com>
2023-02-17 16:03:16 -0500

    added logic in all index.js files

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+router.use('/users', userRoutes);
+router.use('/thoguhts', thoughtRoutes);
+
+module.exports = router;
