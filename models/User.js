c179feccdca494ecb12d42227405bce941892f4e
ejc10d <112822833+ejc10d@users.noreply.github.com>
2023-02-18 09:28:13 -0500

    added user model

--- models/User.js
+++ models/User.js
@@ -1,27 +1,53 @@
 const { Schema, model } = require('mongoose');
-const thought
-
 
 const userSchema = new Schema(
     {
-username: {
-    type: String,
-    required: true,
-    max_length: 50,
-},
-email: {
-    type: String,
-    required: true,
-    unique: true,
-    // must be an actual email
-}
-thoughts: {
-    // array of id values referencing the thought model
-}
-friends
-    // array of id values referencing the user model (self-reference)
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // must match an actual email address
+            match: [/.+@.+\..+/],
+        },
+
+        thoughts:
+            // array of id values referencing the thought model
+            [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Thought',
+                },
+            ],
+        friends:
+            // array of id values referencing the user model (self-reference)
+            [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: 'User',
+                },
+            ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
-)
+);
 
 // virtual "friend count" -  retrieves the lenght of the user's friends array field on query
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
 
