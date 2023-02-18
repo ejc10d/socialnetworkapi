ab38fcd5464eb2f077201c60c9cba99ab831ee2d
ejc10d <112822833+ejc10d@users.noreply.github.com>
2023-02-18 09:10:26 -0500

    added routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+} = require ('../../controllers/thoughtController');
+
+router.route('/').get(getAllThoughts).post(createThought);
+
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/reactionId').delete(removeReaction);
+
+module.exports = router;
